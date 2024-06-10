1d88cd4daf65364408cb3c1953486485da745ccf
owarewaa <akosuaowarewaa360@gmail.com>
2024-06-10 10:50:16 -0700

    SereneSpa

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+const btn = document.getElementById('btn')
+btn.onclick = function () {
+    alert('DM for a relaxing massage')
+}
+
+const btn2 = document.getElementById('btn2')
+btn2.onclick = function () {
+    alert('Discount ends tomorrow @ 6pm')
+}
+
+const btn3 = document.getElementById('btn3')
+btn3.onclick = function () {
+    alert('Back to home')
+}
+
+const btn4 = document.getElementById('btn4')
+btn4.onclick = function () {
+    alert('More reviews')
+}
+
+
